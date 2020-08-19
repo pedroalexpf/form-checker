f5ec41b828121e46d5ceaee7574a367404c23368
Pedro Flores <pedroalex.pf@gmail.com>
2020-08-19 13:13:19 -0300

    feature - disable form out of standart time

--- form-checker.js
+++ form-checker.js
@@ -3,24 +3,23 @@
 window.onload = function horaCerta(){
 
     const d = new Date();
-    let sysTimeSplit = [d.getHours(), d.getMinutes()]; 
+    let sysTimeSplit = [d.getHours(), d.getMinutes()];
     let sysTimeSecs = (sysTimeSplit[0] * 60 * 60) + (sysTimeSplit[1] * 60);
   
     const uTime = "17:00"; // Setar AQUI qual horário de fechamento do Form
-    let uTimeSplit = uTime.split(':'); 
-    let uTimeSecs = (uTimeSplit[0] * 60 * 60) + (uTimeSplit[1] * 60); 
+    let uTimeSplit = uTime.split(':');
+    let uTimeSecs = (uTimeSplit[0] * 60 * 60) + (uTimeSplit[1] * 60);
+    const status = false; //TRUE para desativar as dúvidas fora do horário padrão
+  console.log(status);
+    if( sysTimeSecs >= uTimeSecs || status ){
   
-    if( sysTimeSecs >= uTimeSecs ){
-                    
-          document.getElementById("avisooff").innerHTML='<p style="background:#bc4747; color: #fff; padding: 25px;width: 80%;margin: 0 auto;font-size: larger;font-family: Ubuntu; margin-top:100px;"><strong>Atenção:</strong> No momento o envio de casos aos Especialistas está Off, solicite auxílio aos colegas de time ou à Supervisão.'; 	
-            return document.getElementById("form-duvida").style.display = "none";  		
-           
+          document.getElementById("avisooff").innerHTML='<p style="background:#bc4747; color: #fff; padding: 25px;width: 80%;margin: 0 auto;font-size: larger;font-family: Ubuntu; margin-top:100px;"><strong>Atenção:</strong> No momento o envio de casos aos Especialistas está Off, solicite auxílio aos colegas de time ou à Supervisão.';
+            return document.getElementById("form-duvida").style.display = "none";
+  
       } else {
+  
            document.getElementById("form-duvida").style.display = "block";
           return document.getElementById("avisooff").style.display = "none";
-    
+  
       }
-  }
-  
-  
-  
+  }  
